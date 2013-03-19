6088557753f82a7ef9ba927fa51155f38c515464
bearbotics2358 <bearbotics2358@gmail.com>
2013-03-19 16:44:20 -0500

    Removed Binaries from Commit

--- PPC603gnu/Smokey_VI_partialImage/Debug/Objects/Smokey_VI/BbVirtualGamepad.d
+++
@@ -1,8 +0,0 @@
-Smokey_VI_partialImage/Debug/Objects/Smokey_VI/BbVirtualGamepad.o:  \
- /home/bearbotics/Development/Smokey_VI/BbVirtualGamepad.cpp \
-  /home/bearbotics/Development/Smokey_VI/BbVirtualGamepad.h \
-  /home/bearbotics/Development/Smokey_VI/Framework/Gamepad.h
-
-/home/bearbotics/Development/Smokey_VI/BbVirtualGamepad.h:
-
-/home/bearbotics/Development/Smokey_VI/Framework/Gamepad.h:
